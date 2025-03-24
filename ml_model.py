cc9ddf3b495a4f39c3e7cb0f42887de620f8308b
akshaykumarchincholli <akshaychincholi007@gmail.com>
2025-03-24 21:25:10 +0530

    added few models

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,15 @@
 
 
 print("Adding all ml related code here")
+
+def model1():
+    print("Adding ML Model 1")
+
+def model2():
+    print("Adding ML Model 2")
+
+def model3():
+    print("Adding ML Model 3")
+
+def model4():
+    print("Adding ML Model 4")
